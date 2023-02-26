09aad35c09d1bf1a546add7de4a959699294d0fa
tkdghks4564@naver.com <tkdghks4564@naver.com>
2023-02-26 22:39:03 +0900

    230226_스코프와 Provider_웹이 뜨지 않음

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,43 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        //종료를 실행시켜야 한다면 일일히 넣어줘야함
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+
+        ac.close();
+    }
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
